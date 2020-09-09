85f91cc0e874dcdd8cf7e5d5320ce7a53492e067
Victor El-Hajj <elhajj@student.chalmers.se>
2020-09-09 17:48:28 +0200

    Default page is now Tools page

--- Web/FrontController.hs
+++ Web/FrontController.hs
@@ -11,7 +11,7 @@ import IHP.Welcome.Controller
 
 instance FrontController WebApplication where
     controllers = 
-        [ startPage WelcomeAction
+        [ startPage ToolsAction 
         -- Generator Marker
         , parseRoute @LoansController
         , parseRoute @ToolsController
