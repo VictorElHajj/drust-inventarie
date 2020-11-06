0cacaf39d346a33c159ea56eeb493e1474f9c18e
Victor El-Hajj <elhajj@student.chalmers.se>
2020-11-06 15:54:02 +0100

    Update to latest ihp

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
     ihp = builtins.fetchGit {
         url = "https://github.com/digitallyinduced/ihp.git";
-        rev = "a12a1ce8f16814b802aae39eb26a9d3247192c12";
+        rev = "e21f5d743104ba8b1bed185ee85fb20e064e0bef";
 
     };
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
