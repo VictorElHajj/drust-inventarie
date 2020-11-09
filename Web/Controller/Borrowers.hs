71e45a3cf679a3f2a945603c88843be5235d7a73
VictorElHajj <hajjvictor@live.se>
2020-11-09 21:49:32 +0100

    Only allow logged in user to access borrowers

--- Web/Controller/Borrowers.hs
+++ Web/Controller/Borrowers.hs
@@ -1,55 +1,52 @@
 module Web.Controller.Borrowers where
 
 import Web.Controller.Prelude
+import Web.View.Borrowers.Edit
 import Web.View.Borrowers.Index
 import Web.View.Borrowers.New
-import Web.View.Borrowers.Edit
 import Web.View.Borrowers.Show
 
 instance Controller BorrowersController where
-    action BorrowersAction = do
-        borrowers <- query @Borrower |> fetch
-        render IndexView { .. }
+  beforeAction = ensureIsUser
 
-    action NewBorrowerAction = do
-        let borrower = newRecord
-        render NewView { .. }
+  action BorrowersAction = do
+    borrowers <- query @Borrower |> fetch
+    render IndexView {..}
+  action NewBorrowerAction = do
+    let borrower = newRecord
+    render NewView {..}
+  action ShowBorrowerAction {borrowerId} = do
+    borrower <- fetch borrowerId
+    render ShowView {..}
+  action EditBorrowerAction {borrowerId} = do
+    borrower <- fetch borrowerId
+    render EditView {..}
+  action UpdateBorrowerAction {borrowerId} = do
+    borrower <- fetch borrowerId
+    borrower
+      |> buildBorrower
+      |> ifValid \case
+        Left borrower -> render EditView {..}
+        Right borrower -> do
+          borrower <- borrower |> updateRecord
+          setSuccessMessage "Borrower updated"
+          redirectTo EditBorrowerAction {..}
+  action CreateBorrowerAction = do
+    let borrower = newRecord @Borrower
+    borrower
+      |> buildBorrower
+      |> ifValid \case
+        Left borrower -> render NewView {..}
+        Right borrower -> do
+          borrower <- borrower |> createRecord
+          setSuccessMessage "Borrower created"
+          redirectTo BorrowersAction
+  action DeleteBorrowerAction {borrowerId} = do
+    borrower <- fetch borrowerId
+    deleteRecord borrower
+    setSuccessMessage "Borrower deleted"
+    redirectTo BorrowersAction
 
-    action ShowBorrowerAction { borrowerId } = do
-        borrower <- fetch borrowerId
-        render ShowView { .. }
-
-    action EditBorrowerAction { borrowerId } = do
-        borrower <- fetch borrowerId
-        render EditView { .. }
-
-    action UpdateBorrowerAction { borrowerId } = do
-        borrower <- fetch borrowerId
-        borrower
-            |> buildBorrower
-            |> ifValid \case
-                Left borrower -> render EditView { .. }
-                Right borrower -> do
-                    borrower <- borrower |> updateRecord
-                    setSuccessMessage "Borrower updated"
-                    redirectTo EditBorrowerAction { .. }
-
-    action CreateBorrowerAction = do
-        let borrower = newRecord @Borrower
-        borrower
-            |> buildBorrower
-            |> ifValid \case
-                Left borrower -> render NewView { .. } 
-                Right borrower -> do
-                    borrower <- borrower |> createRecord
-                    setSuccessMessage "Borrower created"
-                    redirectTo BorrowersAction
-
-    action DeleteBorrowerAction { borrowerId } = do
-        borrower <- fetch borrowerId
-        deleteRecord borrower
-        setSuccessMessage "Borrower deleted"
-        redirectTo BorrowersAction
-
-buildBorrower borrower = borrower
-    |> fill @["name","email","phone","lastActive"]
+buildBorrower borrower =
+  borrower
+    |> fill @["name", "email", "phone", "lastActive"]
