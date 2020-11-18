ec4cf4abce4af247643520e73d48cf6c5ef6d0dd
VictorElHajj <hajjvictor@live.se>
2020-11-18 16:48:34 +0100

    More compliant GDPR

--- Web/View/Borrowers/New.hs
+++ Web/View/Borrowers/New.hs
@@ -17,11 +17,12 @@ renderForm borrower =
     [hsx|
     <div class="p-4">
         <h3>Skapa ny lånare</h3>
-        {(textField #name) {fieldLabel = "Namn"} {required = True}}
+        {(textField #name) {fieldLabel = "Fullständigt namn"} {required = True}}
         {(textField #cid) {fieldLabel = "CID"} {required = True}}
         {(textField #email) {fieldLabel = "E-post"} {required = True}}
         {(textField #phone) {fieldLabel = "Telefon"} {required = True}}
-        {(checkboxField #gdprConsent) {fieldLabel = "GDPR: Jag godkänner att DRust sparar denna data tills att nästa DRust går på och alla lånade verktyg är tillbaka."} {required = True}}
+        {(checkboxField #gdprConsent) {fieldLabel = "GDPR: Jag godkänner att DRust sparar denna data tills att nästa DRust går på och alla lånade verktyg är tillbaka. Personuppgifterna sparas så att vi kan nå er ifall ett verktyg ej lämnas tillbaka i rimlig tid."
+} {required = True}}
         {submitButton {label = "Spara"}}
     </div>
 |]
