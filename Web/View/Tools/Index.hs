71d69cd82a541bfa270169e039fef223e151878a
Victor El-Hajj <elhajj@student.chalmers.se>
2021-09-26 15:35:45 +0200

    Add tool search, remove category collapse

--- Web/View/Tools/Index.hs
+++ Web/View/Tools/Index.hs
@@ -8,8 +8,31 @@ data IndexView = IndexView {tools :: [Tool]}
 instance View IndexView where
   html IndexView {..} =
     [hsx|
+        <div class = "p-2" style="background-color: #fa6607;">
+        <input id="toolSearch" type="text" placeholder="Sök verktyg "onkeyup="searchTool()">
+        <script>
+            function searchTool() {
+                searchFor = document.getElementById("toolSearch");
+                filter = searchFor.value.toUpperCase();
+                table = document.getElementById("toolTable");
+                tr = table.getElementsByTagName("tr"); 
+                for (i = 0; i < tr.length; i++) {
+                    if (tr[i].id == "categoryHeader") {continue;}
+                    td = tr[i].getElementsByTagName("td")[1];
+                    if (td) {
+                        txtValue = td.textContent || td.innerText;
+                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                            tr[i].className = "collapse show";
+                        } else {
+                            tr[i].className = "collapse";
+                        }
+                    }
+                }
+            }
+        </script>
+        </div>
         <div class="table-responsive">
-            <table class="table table-sm;" style="border-top:hidden;">
+            <table id="toolTable" class="table table-sm;" style="border-top:hidden;">
                 <thead class="text-light" style="background-color: #fa6607;">
                 <th style="width:15%;">Kategori</th>
                 <th style="width:25%;">Namn</th>
@@ -33,7 +56,7 @@ instance View IndexView where
 
 renderTool category tool =
   [hsx|
-    <tr id={"collapseTool"++(trimSpaces category)} class="collapse" style="transition: none;">
+    <tr id={"collapseTool"++(trimSpaces category)} class="collapse show" style="transition: none;">
         <td></td>
         <td>{get #name tool}</td>
         <td>{get #description tool}</td>
@@ -44,9 +67,9 @@ renderTool category tool =
 
 renderCategory tools category =
   [hsx|
-    <tr style="transform: rotate(0);">
+    <tr id="categoryHeader" style="transform: rotate(0);">
         <th>
-            <a class="btn btn-link btn-block text-left text-dark stretched-link" data-toggle="collapse" data-target={"#collapseTool"++(trimSpaces category)} aria-expanded="false" aria-controls={trimSpaces category}>
+            <a class="text-left text-dark">
             {category}
             </a>
         </th>
